16219a3d72d260d3cc6b5cae0c90869b8aa6fb79
Raissa Frota <raissa_queiroz@hotmail.com>
2022-06-06 20:00:43 -0300

    Criando a primeira classe de domínio

--- src/main/java/com/raissafrota/projetoSpringBoot/resources/CategoriaResource.java
+++ src/main/java/com/raissafrota/projetoSpringBoot/resources/CategoriaResource.java
@@ -1,15 +1,32 @@
 package com.raissafrota.projetoSpringBoot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raissafrota.projetoSpringBoot.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, value = "/boas-vindas" )
 	public String informarQueEstouNaClasseCategoria() {
 		return "Olá! Acessei o endpoint corretamente e estou na classe CategoriaResource! :D";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listarCategorias(){
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;		
 	}
 }
