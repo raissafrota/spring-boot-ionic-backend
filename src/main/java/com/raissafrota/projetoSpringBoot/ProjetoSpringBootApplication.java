0aeea4c62a8230d6eec029f81180e088312cdcf9
Raissa Frota <raissa_queiroz@hotmail.com>
2022-06-08 20:31:47 -0300

    Criando operação de instancialização de Categorias no início da aplicação

--- src/main/java/com/raissafrota/projetoSpringBoot/ProjetoSpringBootApplication.java
+++ src/main/java/com/raissafrota/projetoSpringBoot/ProjetoSpringBootApplication.java
@@ -1,13 +1,31 @@
 package com.raissafrota.projetoSpringBoot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSpringBootApplication {
+import com.raissafrota.projetoSpringBoot.domain.Categoria;
+import com.raissafrota.projetoSpringBoot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoSpringBootApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
