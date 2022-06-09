97715855488c083fb26222ad621989370f0a3c1b
Raissa Frota <raissa_queiroz@hotmail.com>
2022-06-08 21:12:56 -0300

    Criação da classe Produto e da associação com a classe Categoria

--- src/main/java/com/raissafrota/projetoSpringBoot/ProjetoSpringBootApplication.java
+++ src/main/java/com/raissafrota/projetoSpringBoot/ProjetoSpringBootApplication.java
@@ -8,24 +8,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.raissafrota.projetoSpringBoot.domain.Categoria;
+import com.raissafrota.projetoSpringBoot.domain.Produto;
 import com.raissafrota.projetoSpringBoot.repositories.CategoriaRepository;
+import com.raissafrota.projetoSpringBoot.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class ProjetoSpringBootApplication implements CommandLineRunner{
+public class ProjetoSpringBootApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		criarCategoriasAssociadasAosProdutos();
+	}
+
+	private void criarCategoriasAssociadasAosProdutos() {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		this.produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
