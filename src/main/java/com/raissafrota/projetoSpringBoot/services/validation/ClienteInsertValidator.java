137ee5aa7fcdcbf71811ba9fb6243c0551998e19
Raissa Frota <raissa_queiroz@hotmail.com>
2022-11-10 15:29:02 -0300

    Validação customizada para impedir inserir um email de cliente que já foi inserido anteriormente

--- src/main/java/com/raissafrota/projetoSpringBoot/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/raissafrota/projetoSpringBoot/services/validation/ClienteInsertValidator.java
@@ -5,12 +5,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.raissafrota.projetoSpringBoot.domain.Cliente;
 import com.raissafrota.projetoSpringBoot.domain.enums.TipoCliente;
 import com.raissafrota.projetoSpringBoot.dto.ClienteNewDTO;
+import com.raissafrota.projetoSpringBoot.repositories.ClienteRepository;
 import com.raissafrota.projetoSpringBoot.resources.exceptions.FieldMessage;
 import com.raissafrota.projetoSpringBoot.services.validation.utils.ValidationTiposCpfCnpjUtils;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +37,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido!"));
 		}
 
+		Cliente c = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(c != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMensagem()).addPropertyNode(e.getNomeDoCampo())
